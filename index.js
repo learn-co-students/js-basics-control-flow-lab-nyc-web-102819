bb06f7485c8bf713cf5800dd63550a9080cda1bd
Pavel Beletsky <pavel.beletsky@live.com>
2019-12-08 14:46:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let msg
+  if (ride <= 400) {
+    msg = 'This one is on me!';
+  }
+  else if (ride > 2000 && ride < 2500) {
+    msg = 'I will gladly take your thirty bucks.'
+  }
+  else if (ride >= 2500) {
+    msg = 'No can do.'
+  }
+  return msg
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let msg
+  city === 'NYC' ? msg = 'Ok, sounds good.' : msg = 'No go.'
+  return msg
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let msg
+  switch (tip) {
+    case 'generous':
+      msg = "Thank you so much."
+      break;
+
+    case 'not as generous':
+      msg = "Thank you."
+      break;
+  
+    default:
+      msg = "Bye.";
+  }
+
+  return msg
 }
